e78d9887823e12eb3d292cdcc538d7ef8144488b
Thomas <thomas.kuestner@uni-tuebingen.de>
2022-08-23 11:06:34 +0200

    add operators

--- utils/mri.py
+++ utils/mri.py
@@ -1,4 +1,7 @@
 import numpy as np
+#from gpuNUFFT import NUFFTOp
+from merlintf.complex import *
+from mri.operators import NonCartesianFFT
 
 
 def rss(coil_img):
@@ -28,3 +31,52 @@ def fft2c(image, axes=(0,1)):
 
 def ifft2c(kspace, axes=(0,1)):
   return np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(kspace, axes=axes), norm='ortho', axes=axes), axes=axes)
+
+
+# Non-Cartesian 2D operators
+class GPUNUFFTFwd(tf.keras.layers.Layer):
+    def __init__(self, nRead, csm, traj, dcf):
+      super().__init__()
+      self.op = NonCartesianFFT(samples=traj, shape=[nRead, nRead], n_coils=np.shape(csm)[0], density_comp=dcf,
+                                smaps=csm, implementation='gpuNUFFT')
+
+    def call(self, x, csm, traj, dcf):
+      return numpy2tensor(self.op.op(np.squeeze(x.numpy())), add_batch_dim=True, add_channel_dim=False)
+
+
+class GPUNUFFTAdj(tf.keras.layers.Layer):
+  def __init__(self, nRead, csm, traj, dcf):
+    super().__init__()
+    self.op = NonCartesianFFT(samples=traj, shape=[nRead, nRead], n_coils=np.shape(csm)[0], density_comp=dcf, smaps=csm,
+                              implementation='gpuNUFFT')
+
+  def call(self, x, csm, traj, dcf):
+    return numpy2tensor(self.op.adj_op(x), add_batch_dim=True, add_channel_dim=False)
+
+
+# Define Batchelor's motion operator
+def BatchForwardOp(image,smaps,masks,motions):
+  Nx = np.shape(image)[0]
+  Ny = np.shape(image)[1]
+  Nc = np.shape(smaps)[2]
+  Nt = np.shape(masks)[2]
+
+  kspace_out = np.zeros((Nx,Ny,Nc,Nt)) + 1j * np.zeros((Nx,Ny,Nc,Nt))
+  for t in range(Nt):
+    im_aux = apply_sparse_motion(image,get_sparse_motion_matrix(motions[:,:,:,t]),0)
+    kspace_out[:,:,:,t] = mriForwardOp(im_aux, smaps, masks[:,:,t][:,:,np.newaxis])
+  return np.sum(kspace_out,3)
+
+
+def BatchAdjointOp(kspace,smaps,masks,motions):
+  Nx = np.shape(kspace)[0]
+  Ny = np.shape(kspace)[1]
+  Nc = np.shape(smaps)[2]
+  Nt = np.shape(masks)[2]
+
+  image_out = np.zeros((Nx,Ny,Nt)) + 1j * np.zeros((Nx,Ny,Nt))
+  im_aux = np.zeros((Nx,Ny,Nc))
+  for t in range(Nt):
+    im_aux = mriAdjointOp(kspace, smaps, masks[:,:,t][:,:,np.newaxis])
+    image_out[:,:,t] = apply_sparse_motion(im_aux,get_sparse_motion_matrix(motions[:,:,:,t]),1)
+  return np.sum(image_out,2)
